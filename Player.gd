ac09e2a377cbdae559b90a99f8ce265d497fe6b1
MuchMarts.eth <98103825+MuchMarts@users.noreply.github.com>
2024-01-27 12:54:09 +0200

    Basic Heavy/Light attack

--- Player.gd
+++ Player.gd
@@ -3,24 +3,43 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const CROUCH_MODIFIER = 0.5
-@onready var PREV = 0
+const TIME_BETWEEN_KEYS = 0.05 
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var PREV = 0
 @onready var player = get_parent()
 @onready var is_crouch = 0
 @onready var anim = get_node("AnimationPlayer")
-@onready var is_attack = 0
 @onready var attack_type = 0
+@onready var attack_seq_check = 0
+@onready var stored_action = 0
+@onready var time = 0
 
 func attack():
-	match attack_type:
-		0: 
-			anim.play("Light_Attack")
-		1:
-			anim.play("Heavy_Attack")
+	pass
 
+func just_movement():
+	if anim.current_animation == "Idle":
+		return true
+	if anim.current_animation == "Run":
+		return true
+	if anim.current_animation == "Jump":
+		return true
+	if anim.current_animation == "Crouch":
+		return true
+	if anim.current_animation == "Fall":
+		return true
+	# if no animation is playing you can do movement
+	if !anim.is_playing():
+		return true
+	return false
+	
 func _physics_process(delta):
 	var state = 0
+	var direction = 0
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -30,8 +49,12 @@ func _physics_process(delta):
 	var left = Input.is_action_pressed("left")
 	var right = Input.is_action_pressed("right")
 	var down = Input.is_action_pressed("down")
+	var jump = Input.is_action_just_pressed("up")
+	var lAttack = Input.is_action_just_pressed("light_attack")
+	var hAttack = Input.is_action_just_pressed("heavy_attack")
 	
-	var direction = 0
+	
+
 	
 	# Nullify previous input so player cant stop if left and right are pressed at the same time
 	if left != false && right == false:
@@ -46,52 +69,69 @@ func _physics_process(delta):
 			direction = -1
 		elif PREV == -1:
 			direction = 1
-			
-	# Goofy AF crouch
-	var speed = SPEED
-
-	if direction:
-		state = 1
-	if down:
-		state = 4
-		speed *= CROUCH_MODIFIER
-	# Handle jump.
-	if (Input.is_action_just_pressed("up") and is_on_floor()):
-		velocity.y = JUMP_VELOCITY
-	if velocity.y < 0:
-		state = 2
-	if velocity.y > 0:
-		state = 3
-	
-	if Input.is_action_just_pressed("light_attack"):
-		attack_type = 0
-		state = 5
-	if Input.is_action_just_pressed("heavy_attack"):
-		attack_type = 1
-		state = 5
 	
 	if direction == -1:
 		get_node("AnimatedSprite2D").flip_h = true
 	elif direction == 1:
 		get_node("AnimatedSprite2D").flip_h = false
 	
-	match state:
-		0: 
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			anim.play("Idle")
-		1: 
-			velocity.x = direction * speed
+	# Goofy AF crouch
+	
+	if attack_seq_check:
+		time += delta
+		if time <= TIME_BETWEEN_KEYS and (lAttack or hAttack):
+			match stored_action:
+				1:
+					pass
+				2:
+					pass
+				3:
+					pass
+			
+			attack_seq_check = 0
+			stored_action = 0
+	
+	if direction && !attack_seq_check:
+		attack_seq_check = 1
+		stored_action = 1
+		time = 0
+	
+	if jump && !attack_seq_check:
+		attack_seq_check = 1
+		stored_action = 2
+		time = 0
+	
+	if down && !attack_seq_check:
+		attack_seq_check = 1
+		stored_action = 3
+		time = 0
+	
+	
+	if jump and just_movement() and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if direction and just_movement():
+		var speed = SPEED
+		if !down:
 			anim.play("Run")
-		2: 
-			anim.play("Jump")			
-		3: 
-			anim.play("Fall")	
-		4: 
+		else:
 			anim.play("Crouch")
-		5: 
-			attack()
-		_:
-			print("This should not happen")
-			anim.play("Idle")
+			speed *= CROUCH_MODIFIER
+		velocity.x = direction * speed
+	elif down and just_movement():
+		anim.play("Crouch")
+	elif velocity.y > 0 and just_movement():
+		anim.play("Fall")
+	elif velocity.y < 0 and just_movement():
+		anim.play("Jump")
+	elif just_movement():
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim.play("Idle")
+	
+	if hAttack:
+		anim.play("Heavy_Attack")
+	if lAttack:
+		anim.play("Light_Attack")
+	
 	move_and_slide()
 
